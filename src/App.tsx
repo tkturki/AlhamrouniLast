9c3ab838857f37a69085b1d08bdefd5bbc93da6e
tkturki <73724693+tkturki@users.noreply.github.com>
2026-04-26 07:36:54 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import HomePage from './pages/HomePage';
+import AddItemPage from './pages/AddItemPage';
+import SalesPage from './pages/SalesPage';
+import InvoicePage from './pages/InvoicePage';
+import SearchPage from './pages/SearchPage';
+import ItemsPage from './pages/ItemsPage';
+import InvoicesPage from './pages/InvoicesPage';
+import LoginPage from './pages/LoginPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* صفحة تسجيل الدخول بدون Layout */}
+        <Route path="/login" element={<LoginPage />} />
+
+        {/* باقي الصفحات مع Layout */}
+        <Route path="/" element={<Layout><HomePage /></Layout>} />
+        <Route path="/add" element={<Layout><AddItemPage /></Layout>} />
+        <Route path="/sales" element={<Layout><SalesPage /></Layout>} />
+        <Route path="/invoice" element={<Layout><InvoicePage /></Layout>} />
+        <Route path="/search" element={<Layout><SearchPage /></Layout>} />
+        <Route path="/items" element={<Layout><ItemsPage /></Layout>} />
+        <Route path="/invoices" element={<Layout><InvoicesPage /></Layout>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
