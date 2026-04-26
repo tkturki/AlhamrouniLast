f5d2c71cb87f416c491145f2118e4b1db22ddfa5
tkturki <73724693+tkturki@users.noreply.github.com>
2026-04-26 13:19:46 +0200

    Add GalleryPage route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,13 @@ import SearchPage from './pages/SearchPage';
 import ItemsPage from './pages/ItemsPage';
 import InvoicesPage from './pages/InvoicesPage';
 import LoginPage from './pages/LoginPage';
+import GalleryPage from './pages/GalleryPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* طµظپط­ط© طھط³ط¬ظٹظ„ ط§ظ„ط¯ط®ظˆظ„ ط¨ط¯ظˆظ† Layout */}
         <Route path="/login" element={<LoginPage />} />
-
-        {/* ط¨ط§ظ‚ظٹ ط§ظ„طµظپط­ط§طھ ظ…ط¹ Layout */}
         <Route path="/" element={<Layout><HomePage /></Layout>} />
         <Route path="/add" element={<Layout><AddItemPage /></Layout>} />
         <Route path="/sales" element={<Layout><SalesPage /></Layout>} />
@@ -24,6 +22,7 @@ function App() {
         <Route path="/search" element={<Layout><SearchPage /></Layout>} />
         <Route path="/items" element={<Layout><ItemsPage /></Layout>} />
         <Route path="/invoices" element={<Layout><InvoicesPage /></Layout>} />
+        <Route path="/gallery" element={<Layout><GalleryPage /></Layout>} />
       </Routes>
     </BrowserRouter>
   );
